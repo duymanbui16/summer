9e3ba517509d363e9aa5738c3b58e19faa758c84
duymanbui16 <44236369+duymanbui16@users.noreply.github.com>
2024-04-18 13:48:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,30 @@ const question = document.querySelector(".question");
 const gif = document.querySelector(".gif");
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
-const questionPrompt = document.querySelector(".question-prompt"); // Thêm đoạn này
+const questionPrompt = document.querySelector(".question-prompt");
+
+let noButtonClickCount = 0; // Variable to track the number of times "no" button is clicked
 
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "Em bé chạ iu anh 🥺<br>dỗiiii x10000000";
-  gif.src =
-    "https://media.giphy.com/media/vvc1dJPLEU2QOw9cXy/giphy.gif";
-
-  // Ẩn cả hai nút Yes và No
-  yesBtn.style.display = "none";
-  noBtn.style.display = "none";
-  questionPrompt.style.display = "none"; // Ẩn dòng chữ khi người dùng nhấn vào nút Yes
+  gif.src = "https://media.giphy.com/media/vvc1dJPLEU2QOw9cXy/giphy.gif";
+  hideElements();
 });
+
+noBtn.addEventListener("mouseover", () => {
+  noButtonClickCount++; // Increment the count each time "no" button is clicked
+  if (noButtonClickCount > 3) { // Check if the count is more than 3
+    question.innerHTML = "Em bé của anh là đáng iu nhất. Iu em 😘😘😘"; // Change the inner HTML of the question
+    gif.src = "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExeW9kM296MzNld2dyeXMzdGppYnVxeHAxbTN4ZG1keWIweXAwNjNneiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/VXWyVyHi66NhTtpX2S/giphy.gif"; // Set a new source for the GIF
+    hideElements();
+  }
+});
+
+function hideElements() {
+  yesBtn.style.display = "none"; // Hide both buttons and question prompt
+  noBtn.style.display = "none";
+  questionPrompt.style.display = "none";
+}
 
 noBtn.addEventListener("mouseover", () => {
   const noBtnRect = noBtn.getBoundingClientRect();
@@ -27,4 +39,3 @@ noBtn.addEventListener("mouseover", () => {
   noBtn.style.left = randomX + "px";
   noBtn.style.top = randomY + "px";
 });
-
